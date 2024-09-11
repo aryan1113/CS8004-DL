5857ce9a7ecda3c70b13f1a3467f5bb83c7f7ad8
Aryan <aryannewcollege@gmail.com>
2024-09-11 09:52:13 +0530

    Feat: std scaler

--- scaler.py
+++ scaler.py
@@ -12,19 +12,28 @@ def minmax(input_array, axis_param = None):
     '''
     min_val = np.min(input_array, axis = axis_param)
     max_val = np.max(input_array, axis = axis_param)
-    print(min_val, max_val)
-    scaled_array = (input_array - min_val) / (max_val - min_val)
-    return scaled_array
+    
+    minmax_scaled_array = (input_array - min_val) / (max_val - min_val)
+    return minmax_scaled_array
 
-def normalize(input_array):
+def normalize(input_array, axis_param = None):
     '''
     returns normalized version of the input array
     in terms of the z-score
+
+    axis_param = None , considers entire array to be a single array and 
+                    scales accordingly to global mean and global std deviation
+    axis_param = 0 , returns values scaled column wise
+    axis_param = 1, returns values scaled row wise
     '''
-    pass
+    mean_val = np.mean(array, axis = axis_param)
+    std_deviation_val = np.std(array, axis = axis_param)
+
+    normalized_array = ( input_array - mean_val ) / std_deviation_val
+    return normalized_array
 
 array = np.array([[1, 2, 3],
                   [4, 5, 6],
                   [7, 8, 9]])
 
-print(minmax(array))
+print(normalize(array))
